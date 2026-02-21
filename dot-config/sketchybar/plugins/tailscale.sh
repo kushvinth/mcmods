1b821e4323d07b10e8f1278e7c758b3409b1b3e3
kushvinth <kushvinth.m@gmail.com>
2026-02-22 00:59:35 +0530

    Sketchybar(Add): Fancy Animations

--- dot-config/sketchybar/plugins/tailscale.sh
+++ dot-config/sketchybar/plugins/tailscale.sh
@@ -1,24 +1,29 @@
 #!/bin/bash
 # inspo: https://github.com/kejadlen/dotfiles/blob/7eac34262edfab1b6774c158de2f83c0b26a363c/.config/sketchybar/plugins/tailscale.sh
 source "$CONFIG_DIR/colors.sh"
+source "$CONFIG_DIR/icons.sh"
+
+LOCK_ICON=􀎡
+UNLOCK_ICON=􀎥
 
 # Toggle tailscale on click
 if [ "$SENDER" = "mouse.clicked" ]; then
 	if tailscale status --self &>/dev/null; then
-		tailscale down 
+		# Currently connected, disconnecting - show unlock animation
+		sketchybar --set "$NAME" background.image.scale=0 icon.drawing=on icon="$UNLOCK_ICON" icon.color=$RED
+		sketchybar --animate sin 15 --set "$NAME" icon.color=$GREY
+		tailscale down
+		sleep 1
+		sketchybar --set "$NAME" icon.drawing=off background.image.scale=0.03
 	else
+		# Currently disconnected, connecting - show lock animation
 		tailscale up
+		sleep 0.5
+		sketchybar --set "$NAME" background.image.scale=0 icon.drawing=on icon="$LOCK_ICON" icon.color=$GREY
+		sketchybar --animate sin 15 --set "$NAME" icon.color=$GREEN
+		sleep 1
+		sketchybar --set "$NAME" icon.drawing=off background.image.scale=0.03
 	fi
-	sleep 0.5  # Wait for status to update
 fi
 
-# Update icon based on status
-if tailscale status --self &>/dev/null; then
-	ICON=􀎡
-	ICON_COLOR=$GREEN
-else
-	ICON=􀎥
-	ICON_COLOR=$GREY
-fi
-
-sketchybar --set "$NAME" icon="$ICON" icon.color="$ICON_COLOR"
+sketchybar --set "$NAME"
